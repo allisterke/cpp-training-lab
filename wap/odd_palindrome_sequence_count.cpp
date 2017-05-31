0671d91c4b2cb167bf1835f27e243dd067f0d67b
ally <allisterke@gmail.com>
2017-05-31 20:41:11 +0800

    no need to use two matrix

--- wap/odd_palindrome_sequence_count.cpp
+++ wap/odd_palindrome_sequence_count.cpp
@@ -6,19 +6,23 @@ long long int MOD = 1e9+7;
 int main() {
 	string s;
 	cin >> s;
-	vector<vector<int>> fc(s.size(), vector<int>(s.size()));
+//	vector<vector<int>> fc(s.size(), vector<int>(s.size()));
 	vector<vector<int>> rc(s.size(), vector<int>(s.size()));
 	for(int k = 0; k < s.size(); ++ k) {
 		for(int i = 0; i + k < s.size(); ++ i) {
 			if(k == 0) {
-				fc[i][i+k] = 1;
+//				fc[i][i+k] = 1;
 				rc[i][i+k] = 1;
 			}
 			else {
-				fc[i][i+k] = k > 1 && s[i] == s[i+k] ? rc[i+1][i+k-1] : 0;
-				rc[i][i+k] = rc[i][i+k-1] + rc[i+1][i+k] - rc[i+1][i+k-1] + fc[i][i+k];
+//				fc[i][i+k] = k > 1 && s[i] == s[i+k] ? rc[i+1][i+k-1] : 0;
+//				rc[i][i+k] = rc[i][i+k-1] + rc[i+1][i+k] - rc[i+1][i+k-1] + fc[i][i+k];
+				rc[i][i+k] = rc[i][i+k-1] + rc[i+1][i+k];
+                if(s[i] != s[i+k]) {
+                    rc[i][i+k] -= rc[i+1][i+k-1];
+                }
 			}
-			fc[i][i+k] %= MOD;
+//			fc[i][i+k] %= MOD;
 			rc[i][i+k] %= MOD;
 		}
 	}
